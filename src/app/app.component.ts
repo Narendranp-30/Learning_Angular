3b565c6270aca8e30116ef37237d77568c6904dd
Narendranp-30 <nprasanth372@gmail.com>
2025-06-22 16:37:26 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
+
 export class AppComponent {
   title = 'AngularDemo';
+  myName(){
+  return "Narendra"
 }
+imageurl="https://picsum.photos/200/300";
+imagealt="Mylogo";
+demo = "Property Binding"
+
+//safe:any=null;
+safe = {
+  name:"safe navigation",
+  working:"yes",
+  time:"now"
+
+}
+
+count=0;
+counter(){
+  this.count++;
+}
+
+
+}
+
