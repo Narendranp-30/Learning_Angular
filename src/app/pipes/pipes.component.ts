9ac89d88d977f7cc79d9d272485a38cdc62dd44e
Narendranp-30 <nprasanth372@gmail.com>
2025-07-06 14:16:01 +0530

    Learning Pipes

--- src/app/pipes/pipes.component.ts
+++ src/app/pipes/pipes.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pipes.component.html',
   styleUrl: './pipes.component.css'
 })
 export class PipesComponent {
 
+  title='pipes';
+  date=new Date();
+  item = ['i1','i2','i3','i4','i5']
+  demo="Good Morning";
 }
